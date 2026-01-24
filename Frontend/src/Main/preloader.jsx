d1cdf5240e9e1d3b2e532a788dc8e4fe560f6f17
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-01-24 11:35:36 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/src/Main/preloader.jsx
+++ Frontend/src/Main/preloader.jsx
@@ -1,23 +1,75 @@
-import { useRef } from "react"
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
-const Preloader = () => {
-    useGSAP(() => {
+const Preloader = ({ onFinish }) => {
+    useGSAP(
+        () => {
+            const tl = gsap.timeline({
+
+                onComplete: () => {
+                    onFinish?.(); // notify parent safely
+                },
+            });
+            tl.from(".text-1", {
+                x: -500,
+                duration: 1.5,
+                opacity: 0,
+                ease: "power4.out",
+            })
+            tl.from(
+                ".text-2",
+                {
+                    x: 500,
+                    duration: 1.5,
+                    opacity: 0,
+                    ease: "power4.out",
+                },
+                "<" // same time
+            )
+            tl.to(".text-1", {
+
+                x: -500,
+                duration: 1.5,
+                opacity: 0,
+                ease: "power4.in",
+            })
+            tl.to(
+                ".text-2",
+                {
+                    x: +500,
+                    duration: 1.5,
+                    opacity: 0,
+                    ease: "power4.in",
+                },
+                "<" // same time
+            )
 
 
-        gsap.to(".preStair", {
-            height: 0,
-            duration: 1,
-            stagger: 0.1,
-            ease: "power4.in",
-            delay: 3
-        })
+            tl.to(".preStair", {
+                height: 0,
 
-    })
+                duration: 1.5,
+                stagger: 0.1,
+                ease: "power4.in",
+            })
+
+
+        }
+    );
 
     return (
-        <div className="fixed inset-0 z-50 flex h-screen w-screen">
+        <div
+
+            className="fixed inset-0 z-50 flex h-screen w-screen"
+        >
+            {/* Center Logo */}
+            <div className=" absolute inset-0 z-50 bg-white/200 font-Logo-font text-white flex justify-center items-center gap-11 ">
+                <div className="text-1 text-[12rem] font-extrabold">Code</div>
+                <div className="text-2 text-[12rem] font-extrabold">Sarthi</div>
+            </div>
+
+            {/* Stair Animation */}
             <div className="preStair w-1/6 bg-black"></div>
             <div className="preStair w-1/6 bg-black"></div>
             <div className="preStair w-1/6 bg-black"></div>
@@ -25,7 +77,7 @@ const Preloader = () => {
             <div className="preStair w-1/6 bg-black"></div>
             <div className="preStair w-1/6 bg-black"></div>
         </div>
-    )
-}
+    );
+};
 
-export default Preloader
+export default Preloader;
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         "circular-web": ["circular-web", "sans-serif"],
         "robert-medium": ["robert-medium", "sans-serif"],
         "robert-regular": ["robert-regular", "sans-serif"],
+        "Logo-font": ["Logo-font", "sans-serif"],
       },
       colors: {
         blue: {
