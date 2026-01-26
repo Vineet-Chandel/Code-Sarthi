31dcc323591849f1d641fcd269e1114bff28245b
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-01-26 19:48:01 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/src/Main/Footer.jsx
+++ Frontend/src/Main/Footer.jsx
@@ -2,82 +2,115 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="
-            h-screen 
-            w-full 
-            flex flex-col
-            bg-black
-            overflow-hidden
-             bg-gradient-to-r from-blue-500/20 to-purple-500/20 
-        ">
-            <div className="
-                h-1/2 
-                border-b border-gray-800
-                flex items-end justify-center pb-12
-            ">
+        <div
+            className="
+    min-h-screen
+    w-full
+    flex flex-col
+    bg-black
+    overflow-hidden
+    bg-gradient-to-r from-blue-500/20 to-purple-500/20
+  "
+        >
+            {/* TOP HALF */}
+            <div
+                className="
+      flex-1
+      border-b border-gray-800
+      flex items-end justify-center
+      pb-12
+      max-sm:pb-6
+    "
+            />
 
-            </div>
-
-            <div className="
-                h-1/2
-                text-white
-                flex justify-center items-center
-                font-Logo-font
-                font-extrabold
-                text-[18rem] md:text-[22rem] lg:text-[25rem] xl:text-[28rem]
-                tracking-[-0.02em]
-                leading-[0.8]
-                
-                relative
-                group
-            ">
+            {/* BOTTOM HALF */}
+            <div
+                className="
+      flex-1
+      text-white
+      flex justify-center items-center
+      font-Logo-font font-extrabold
+      relative group
+      leading-[0.85]
+    "
+            >
                 {/* Glow effect */}
-                <div className="
-                    absolute 
-                    inset-0 
-                    blur-3xl 
-                    group-hover:blur-4xl 
-                    transition-all 
-                    duration-500
-                "></div>
+                <div
+                    className="
+        absolute inset-0
+        bg-white/10
+        blur-3xl
+        group-hover:blur-[120px]
+        transition-all duration-500
+      "
+                />
 
-                {/* Main text */}
-                <div className="relative z-10 text-[18rem] tracking-[0.1rem] font-Logo-font">
-                    <div className='flex'>
-                        <div className='flex'>
-                            <div className='hover:opacity-50 transistion-all duration-300 '>C</div>
-                            <div className='hover:opacity-50 transistion-all duration-300 '>o</div>
-                            <div className='hover:opacity-50 transistion-all duration-300 '>d</div>
-                            <div className='hover:opacity-50 transistion-all duration-300 '>e</div>
-                        </div>
+                {/* MAIN TEXT */}
+                <div
+                    className="
+        relative z-10
+        flex
+        text-[5rem]
+        sm:text-[7rem]
+        md:text-[10rem]
+        lg:text-[14rem]
+        xl:text-[18rem]
+        2xl:text-[22rem]
+        tracking-[0.05em]
+        max-sm:tracking-[0.02em]
+      "
+                >
+                    {/* CODE */}
+                    <div className="flex">
+                        {["C", "o", "d", "e"].map((char, i) => (
+                            <span
+                                key={i}
+                                className="
+              transition-opacity duration-300
+              hover:opacity-50
+              max-sm:hover:opacity-100
+            "
+                            >
+                                {char}
+                            </span>
+                        ))}
+                    </div>
 
-                        <div className='flex'>
-                            <div className='hover:opacity-50 transistion-all duration-300 '>S</div>
-                            <div className='hover:opacity-50 transistion-all duration-300 '>a</div>
-                            <div className='hover:opacity-50 transistion-all duration-300 '>r</div>
-                            <div className='hover:opacity-50 transistion-all duration-300 '>t</div>
-                            <div className='hover:opacity-50 transistion-all duration-300 '>h</div>
-                            <div className='hover:opacity-50 transistion-all duration-300 '>i</div>
-                        </div>
-
+                    {/* SARTHI */}
+                    <div className="flex ml-6 max-sm:ml-3">
+                        {["S", "a", "r", "t", "h", "i"].map((char, i) => (
+                            <span
+                                key={i}
+                                className="
+              transition-opacity duration-300
+              hover:opacity-50
+              max-sm:hover:opacity-100
+            "
+                            >
+                                {char}
+                            </span>
+                        ))}
                     </div>
                 </div>
 
-                {/* Subtle hint */}
-                <div className="
-                    absolute 
-                    bottom-4 
-                    right-4 
-                    text-gray-500 
-                    text-xs 
-                    tracking-[0.5em]
-                    rotate-90
-                    origin-bottom-right
-                ">
+                {/* SCROLL HINT */}
+                <div
+                    className="
+        absolute
+        bottom-4 right-4
+        text-gray-500
+        text-[10px]
+        tracking-[0.4em]
+        rotate-90
+        origin-bottom-right
+        hidden sm:block
+      "
+                >
                     SCROLL
                 </div>
             </div>
         </div>
+
     )
 }
 
