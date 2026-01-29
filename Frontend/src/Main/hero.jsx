33fa42b6d47f9bcf906484158b4c5a0587261cfe
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-01-29 21:03:41 +0530

    chore: clean up code structure and remove unused code blocks

--- Frontend/src/Main/hero.jsx
+++ Frontend/src/Main/hero.jsx
@@ -3,18 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mainhero from "./main-hero";
 import Nav from "./nav";
 import { useState } from "react";
-
 import Preloader from "./preloader";
 import Footer from "./Footer";
 import ContentFirst from "./ContentFirst";
 const Hero = () => {
-    // const [loading, setLoading] = useState(true)
-    // setInterval(() => {
-    //     setLoading(false);
-    // }, 5000);
+    const [loading, setLoading] = useState(true)
+    setInterval(() => {
+        setLoading(false);
+    }, 5000);
     return (
         <div>
-            {/* {loading && <Preloader loading />} */}
+            {loading && <Preloader loading />}
             <Nav />
             <Mainhero />
             <ContentFirst />
