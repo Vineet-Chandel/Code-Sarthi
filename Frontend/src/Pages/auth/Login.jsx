65c81f1782670064975b79e26fcb7ddf6a4e4f61
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-03-14 19:51:01 +0530

    gege

--- Frontend/src/Pages/auth/Login.jsx
+++ Frontend/src/Pages/auth/Login.jsx
@@ -17,8 +17,8 @@ const Login = () => {
      default gmail = vineetsinghbitu@gmail.com
      default password = Vineet@1234
      */
-    const [gmail, setGmailId] = useState("vineetsinghbitu@gmail.com");
-    const [password, setPassword] = useState("Vineet@1234");
+    const [gmail, setGmailId] = useState("");
+    const [password, setPassword] = useState("");
 
     const [showPassword, setShowPassword] = useState(false);
     const [newError, setNewError] = useState(false);
