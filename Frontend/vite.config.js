eb4c99273d00659a73f7adbbe6b2d4e5e4b25dab
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-04-10 21:29:48 +0530

    refactor: remove host and port configuration from vite preview settings

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,
-    port: process.env.PORT,
     allowedHosts: "all"
   }
 })
