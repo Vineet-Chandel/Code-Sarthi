b1876d538cbe78c48d020a0093ff0dbb8d42f4bc
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-04-10 20:35:56 +0530

    refactor: replace bcrypt with bcryptjs for cross-platform compatibility

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose; //“We are extracting the Schema property from the
 
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 
 
--- Backend/src/routes/profRouter.js
+++ Backend/src/routes/profRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const profileRouter = express.Router();
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { userAuth } = require("../middlewares/userAuth");
 const { validateEditProfileData } = require("../utils/validation");
 const validator = require("validator");
