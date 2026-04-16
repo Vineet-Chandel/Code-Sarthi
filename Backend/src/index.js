ec0579e7367d09c5d98658b89671aa8bd9a5d4bf
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-04-16 22:22:06 +0530

    refactor: update CORS origin, prefix API routes, and switch base URL to production endpoint

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: process.env.AT_FRONT,
-        // origin: process.env.AT_SYSTEM_API,
+        // origin: process.env.AT_FRONT,
+        origin: process.env.AT_SYSTEM_API,
         credentials: true
     })
 );
@@ -37,22 +37,22 @@ cloudinary.config({
 });
 
 
-// app.use("/api", authRouter);
-// app.use("/api", profileRouter);
-// app.use("/api", requestRouter);
-// app.use("/api", userPreference);
-// app.use("/api", passwordManagment);
-// app.use("/api", fileUpload);
-// app.use("/", chatRouter);
-
-
-app.use("/", authRouter);
-app.use("/", profileRouter);
-app.use("/", requestRouter);
-app.use("/", userPreference);
-app.use("/", passwordManagment);
-app.use("/", fileUpload);
+app.use("/api", authRouter);
+app.use("/api", profileRouter);
+app.use("/api", requestRouter);
+app.use("/api", userPreference);
+app.use("/api", passwordManagment);
+app.use("/api", fileUpload);
 app.use("/", chatRouter);
+
+
+// app.use("/", authRouter);
+// app.use("/", profileRouter);
+// app.use("/", requestRouter);
+// app.use("/", userPreference);
+// app.use("/", passwordManagment);
+// app.use("/", fileUpload);
+// app.use("/", chatRouter);
 
 const PORT = process.env.PORT || 8000;
 
