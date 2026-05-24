1b2dd12828117b1adc8d071b75f15b5433173d06
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-05-24 21:49:34 +0530

    kuch

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,9 @@ export default defineConfig({
       "www.codesarthi.in",
       "codesarthi.in",
       "https://codesarthi.in",
-      "https://www.codesarthi.in"
+      "https://www.codesarthi.in",
+      "code-sarthi-main-backend.onrender.com",
+      "https://code-sarthi-main-backend.onrender.com"
     ]
   },
   // Add this block to bypass the broken fsevents macOS binary
