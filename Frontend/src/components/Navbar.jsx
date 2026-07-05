73734e6f8250f83b86f829d05fa1110ae5d893bf
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-07-05 08:32:29 +0530

    feat: update local development configurations and improve navbar responsiveness and styling

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -298,17 +298,17 @@ const NavBar = ({ setSelectedChatUser, selectedChatUser }) => {
 
 
 
-                                <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24">
+                                <svg className="h-4 sm:h-5 w-4 sm:w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                     <path fill="currentColor" d="M2 6.21c0-1.984 0-2.977.659-3.593S4.379 2 6.5 2s3.182 0 3.841.617C11 3.233 11 4.226 11 6.21v11.58c0 1.984 0 2.977-.659 3.593S8.621 22 6.5 22s-3.182 0-3.841-.617C2 20.767 2 19.774 2 17.79z" opacity={0.5}></path>
                                     <path fill="currentColor" d="M13 15.4c0-2.074 0-3.111.659-3.756S15.379 11 17.5 11s3.182 0 3.841.644C22 12.29 22 13.326 22 15.4v2.2c0 2.074 0 3.111-.659 3.756S19.621 22 17.5 22s-3.182 0-3.841-.644C13 20.71 13 19.674 13 17.6zm0-9.9c0-1.087 0-1.63.171-2.06a2.3 2.3 0 0 1 1.218-1.262C14.802 2 15.327 2 16.375 2h2.25c1.048 0 1.573 0 1.986.178c.551.236.99.69 1.218 1.262c.171.43.171.973.171 2.06s0 1.63-.171 2.06a2.3 2.3 0 0 1-1.218 1.262C20.198 9 19.673 9 18.625 9h-2.25c-1.048 0-1.573 0-1.986-.178a2.3 2.3 0 0 1-1.218-1.262C13 7.13 13 6.587 13 5.5"></path>
                                 </svg>
 
                             </div>
                         </button>
-                        <svg xmlns="http://www.w3.org/2000/svg" width={30} height={30} className="text-info " viewBox="0 0 12 24"><defs><path id="SVG1pzpbdYY" fill="#000" d="m7.588 12.43l-1.061 1.06L.748 7.713a.996.996 0 0 1 0-1.413L6.527.52l1.06 1.06l-5.424 5.425z"></path></defs><use fillRule="evenodd" href="#SVG1pzpbdYY" transform="rotate(-180 5.02 9.505)"></use></svg>
-                        <div className="name font-semibold text-xl ml-[-8px] text-black relative w-fit after:absolute after:left-0 after:bottom-0 
+                        <svg className="h-6 sm:h-10 w-6  sm:w-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 24"><defs><path id="SVG1pzpbdYY" fill="#000" d="m7.588 12.43l-1.061 1.06L.748 7.713a.996.996 0 0 1 0-1.413L6.527.52l1.06 1.06l-5.424 5.425z"></path></defs><use fillRule="evenodd" href="#SVG1pzpbdYY" transform="rotate(-180 5.02 9.505)"></use></svg>
+                        <div className="name font-semibold text-md sm:text-xl ml-[-8px] text-black relative w-fit after:absolute after:left-0 after:bottom-0 
 after:h-[2px] after:w-0 after:bg-black after:transition-all decoration-[3px]
-group-hover:after:w-full">
+group-hover:after:w-full font-bold font-poppins">
                             {activePage}
                         </div>
                     </div>
@@ -371,13 +371,13 @@ group-hover:after:w-full">
                             </button>
 
                             <span className="h-10 flex items-center animate-pulse ">
-                                <svg className="rotate-[270deg]" xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24">
+                                <svg className="rotate-[270deg] w-4 h-4 sm:w-5 sm:h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                     <path fill="#fff" d="M16.21 16H7.79a1.76 1.76 0 0 1-1.59-1a2.1 2.1 0 0 1 .26-2.21l4.21-5.1a1.76 1.76 0 0 1 2.66 0l4.21 5.1A2.1 2.1 0 0 1 17.8 15a1.76 1.76 0 0 1-1.59 1"></path>
                                 </svg>
-                                <svg className="rotate-[270deg]" xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24">
+                                <svg className="rotate-[270deg] w-4 h-4 sm:w-5 sm:h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                     <path fill="#fff" d="M16.21 16H7.79a1.76 1.76 0 0 1-1.59-1a2.1 2.1 0 0 1 .26-2.21l4.21-5.1a1.76 1.76 0 0 1 2.66 0l4.21 5.1A2.1 2.1 0 0 1 17.8 15a1.76 1.76 0 0 1-1.59 1"></path>
                                 </svg>
-                                <svg className="rotate-[270deg]" xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24">
+                                <svg className="rotate-[270deg] w-4 h-4 sm:w-5 sm:h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                     <path fill="#fff" d="M16.21 16H7.79a1.76 1.76 0 0 1-1.59-1a2.1 2.1 0 0 1 .26-2.21l4.21-5.1a1.76 1.76 0 0 1 2.66 0l4.21 5.1A2.1 2.1 0 0 1 17.8 15a1.76 1.76 0 0 1-1.59 1"></path>
                                 </svg>
                             </span>
