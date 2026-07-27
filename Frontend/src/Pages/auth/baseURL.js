c3bd8a549e3e2869e9c2b4c38e426a95b030513a
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-07-27 14:29:17 +0530

    chore: update CORS origin and API base URL for production deployment

--- Frontend/src/Pages/auth/baseURL.js
+++ Frontend/src/Pages/auth/baseURL.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://code-sarthi-main-backend.onrender.com/api";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://code-sarthi-main-backend.onrender.com/api";
+// const BASE_URL = "http://localhost:3000";
 
 export default BASE_URL;
