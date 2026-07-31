7c57f8b0049fb2b3ac616be04dffd6ccad0192de
VINEET CHANDEL <vineetsinghbitu@gmail.com>
2026-07-31 21:40:26 +0530

    chore: update BASE_URL to point to production domain

--- Frontend/src/Pages/auth/baseURL.js
+++ Frontend/src/Pages/auth/baseURL.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://code-sarthi-main-backend.onrender.com/api";
+const BASE_URL = "https://codesarthi.in/api";
 // const BASE_URL = "http://localhost:3000";
 
 export default BASE_URL;
